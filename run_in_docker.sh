a571e0dc1659225cbb9258ee4c276b136eca44f9
Dao Heng Liu <daohengliu@gmail.com>
2023-05-27 06:46:06 +0100

    update run script

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 docker build --tag "waifujam" .
+docker stop gunicorn || true
 docker run --name gunicorn -p 80:80 -d --rm "waifujam" gunicorn main:app --workers 6 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
