84a2c198d092823f14a2f10c4e13ebfb8f80caa5
Dao Heng Liu <daohengliu@gmail.com>
2023-05-29 22:21:55 +0100

    remove extraneous console.log

--- sample_frontend/cdc/index.js
+++ sample_frontend/cdc/index.js
@@ -1,34 +1,23 @@
-// paste your api v1 token into data/apiv1token.txt
+// paste your api v1 token into data/apiv1token.txt, NOT HERE
 let apiV1Token = ""
+const playersDataJson = 'data/players.json'
+const corsProxyBaseUrl = `http://localhost:8010/proxy`
 
 fetch("data/apiv1token.txt").then(async response => {
     apiV1Token = await response.text();
-    console.log(apiV1Token);
 
     populatePlayers();
 })
 
 
-
-const playersDataJson = 'data/players.json'
-
-// noinspection HttpUrlsUsage
-const baseUrl = `http://${window.location.host}`
-const corsProxyBaseUrl = `http://localhost:8010/proxy`
-
-
-
 const playersContainer = document.getElementById("player-container-left");
 const playerTemplate = document.getElementById("player-template");
-
-console.log(playersContainer);
 
 
 function populatePlayers() {
     fetch(playersDataJson).then(response => response.json()).then(async data => {
-    // console.log(data);
     for (const playerId of data) {
-        console.log(playerId);
+        // console.log(playerId);
         const newNode = playerTemplate.content.cloneNode(true);
 
         try {
@@ -36,7 +25,7 @@ function populatePlayers() {
                 `${corsProxyBaseUrl}/api/get_user?k=${apiV1Token}&u=${playerId}&type=id`
             )
             let playerData = await response.json();
-            console.log(playerData);
+            // console.log(playerData);
             newNode.querySelectorAll("span")[0].innerText = playerData[0].username;
             newNode.querySelectorAll("span")[1].innerText = `Rank #${playerData[0].pp_rank}`;
 
