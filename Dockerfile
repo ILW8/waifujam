ed52701559c189d2fc227dc099b86bffe428f0ce
Dao Heng Liu <daohengliu@gmail.com>
2023-05-29 22:02:27 +0100

    remove wj files

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM ubuntu:22.04
-RUN apt-get update && apt-get install -y python3 python3-pip python3-pymysql ca-certificates git
-
-ADD requirements.txt /requirements.txt
-RUN mkdir -p /waifujam/
-WORKDIR /waifujam
-RUN python3 -m pip install -r /requirements.txt
-COPY . /waifujam
--- run_in_docker.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build --tag "waifujam" .
-docker stop gunicorn || true
-docker wait gunicorn; docker run --name gunicorn -p 80:80 -d --rm "waifujam" gunicorn main:app --workers 6 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
-docker logs -f gunicorn
